1997864d7645da15c0e5a47a4e111a89668594d4
Chethan <chethanfortest@gmail.com>
2017-09-08 01:38:34 +0530

    #added wiki

--- app.py
+++ app.py
@@ -86,7 +86,7 @@ def processwikiaction(req):
 def makeWikiQuery(req):
     result = req.get("result")
     parameters = result.get("parameters")
-    query = parameters.get("given-name")
+    query = parameters.get("any")
     if query is None:
         return None
 
